5d5f919c5e65ed95b7c06fb79f97fb863c050c38
Gregory Vincic <g@7de.se>
2020-02-01 17:47:05 +0100

    Refactor Term tests

--- term_test.go
+++ term_test.go
@@ -10,45 +10,34 @@ import (
 
 func TestTerm(t *testing.T) {
 	m := NewTerm()
-	ok := assertOk(t)
-	ok(loggerSet(m))
+	ok, _k := assert(t)
 	ok(silentLog(m))
 
 	m.Logger = t
 	m.exit = func(int) {}
 	m.Verbose = true
-	bad := assertBad(t)
-	bad(silentLog(m))
+
+	_k(silentLog(m))
 	m.Shf("%s", "touch term_test.go")
-	bad(m.Shf("%s", "hubladuble"))
+	_k(m.Shf("%s", "hubladuble"))
 	// output is trimmed
 	m.Sh("echo '  hello '")
 }
 
 func TestColor(t *testing.T) {
 	line := "/home/john"
-	ok := assertOk(t)
+	ok, _k := assert(t)
 	ok(Color(&line, "/home"))
-
-	bad := assertBad(t)
-	bad(Color(&line, "/etc"))
+	_k(Color(&line, "/etc"))
 }
 
 func TestStrip(t *testing.T) {
 	line := "/home/john"
-	ok := assertOk(t)
+	ok, _k := assert(t)
 	ok(Strip(&line, "/home"))
 
 	line2 := "/home/john"
-	bad := assertBad(t)
-	bad(Strip(&line2, "/etc"))
-}
-
-func loggerSet(m *Term) error {
-	if m.Logger == nil {
-		return fmt.Errorf("Logger is nil")
-	}
-	return nil
+	_k(Strip(&line2, "/etc"))
 }
 
 func silentLog(m *Term) error {

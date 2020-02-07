22ef98972d9e1c071bf8b89da7cadc231e1d1bc3
Gregory Vincic <g@7de.se>
2020-02-07 05:37:48 +0100

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Package f provides a terminal wrapper for my workflows.
+
+  import . "github.com/gregoryv/f"
+
+  func main() {
+    Sh("echo Hello, world!")
+    Shf("echo %s", "Hello, world!")
+  }
+
+*/
+package f
